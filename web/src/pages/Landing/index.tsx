9c61258724ff7725e7cc133a959402dee16414af
Alexandre Teixeira <mrtalexandre@gmail.com>
2020-08-04 23:48:41 -0300

    Refactir landing page to use react router

--- web/src/pages/Landing/index.tsx
+++ web/src/pages/Landing/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import logoImg from '../../assets/images/logo.svg';
 import landingImg from '../../assets/images/landing.svg';
 import studyIcon from '../../assets/images/icons/study.svg';
 import giveClassesIcon from '../../assets/images/icons/give-classes.svg';
 import purpleHeartIcon from '../../assets/images/icons/purple-heart.svg';
 import './styles.css';
+import TeacherForm from '../TeacherForm';
 
 function Landing() {
   return (
@@ -18,15 +20,15 @@ function Landing() {
         <img src={landingImg} alt="Plataforma de estudos" className="hero-image"/>
 
         <div className="buttons-container">
-          <a href="" className="study">
+          <Link to="/study" className="study">
             <img src={studyIcon} alt="Estudar"/>
             Estudar
-          </a>
+          </Link>
 
-          <a href="" className="give-classes">
+          <Link to="/give-classes" className="give-classes">
             <img src={giveClassesIcon} alt="Dar aulas"/>
             Dar aulas
-          </a>
+          </Link>
         </div>
 
         <span className="total-connections">
